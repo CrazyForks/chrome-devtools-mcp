7ffdc5ee4d9df9f62f03354fa758fb4d022c3b08
Alex Rudenko <OrKoN@users.noreply.github.com>
2026-02-23 19:12:29 +0100

    feat: add take_memory_snapshot tool (#1023)
    Refs: https://github.com/ChromeDevTools/chrome-devtools-mcp/issues/406

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -8,6 +8,7 @@ import * as consoleTools from './console.js';
 import * as emulationTools from './emulation.js';
 import * as extensionTools from './extensions.js';
 import * as inputTools from './input.js';
+import * as memoryTools from './memory.js';
 import * as networkTools from './network.js';
 import * as pagesTools from './pages.js';
 import * as performanceTools from './performance.js';
@@ -22,6 +23,7 @@ const tools = [
   ...Object.values(emulationTools),
   ...Object.values(extensionTools),
   ...Object.values(inputTools),
+  ...Object.values(memoryTools),
   ...Object.values(networkTools),
   ...Object.values(pagesTools),
   ...Object.values(performanceTools),
